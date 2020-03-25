29d364fe9a169b9e92f6af111f3cee7df15a1f44
David Ketcheson <dketch@gmail.com>
2020-03-25 09:33:56 +0300

    Update routines to load from new JHU files.
    No more recovered counts so we must model them.
    US is now a single line.

--- SIR.py
+++ SIR.py
@@ -4,55 +4,37 @@ import matplotlib
 from ipywidgets import interact, widgets
 import matplotlib.dates as dates
 from scipy.integrate import solve_ivp
-import pandas as pd
 from IPython.display import Image
 plt.style.use('seaborn-poster')
 matplotlib.rcParams['figure.figsize'] = (10., 6.)
+from data import population, jhu_data, load_cases
 
-url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
-cases = pd.read_csv(url)
-url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
-recovered = pd.read_csv(url)
-url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
-deaths = pd.read_csv(url)
-today = cases.columns[-1]
-days = pd.date_range(start='1/22/20',end=today)
-dd = np.arange(len(days))
+cases, deaths, today, days = jhu_data()
 
-def load_cases(region):
-    if region == 'World':
-        rows = list(range(len(cases.index)))
-    elif region == 'US':
-        tophalf = cases.iloc[:200]  # In lower part of file, duplicate data is given for cities
-        rows = tophalf['Country/Region'].isin([region])
-    elif region in ['Hubei']:
-        rows = cases['Province/State'].isin([region])
-    else:
-        rows = cases['Country/Region'].isin([region])
-        
-    if region == 'US':
-        total_cases = [cases.iloc[:200][day.strftime('%-m/%-d/%y')][rows].sum() for day in days]
-        total_recovered = [recovered.iloc[:200][day.strftime('%-m/%-d/%y')][rows].sum() for day in days]
-        total_deaths = [deaths.iloc[:200][day.strftime('%-m/%-d/%y')][rows].sum() for day in days]        
-    else:
-        total_cases = [cases[day.strftime('%-m/%-d/%y')][rows].sum() for day in days]
-        total_recovered = [recovered[day.strftime('%-m/%-d/%y')][rows].sum() for day in days]
-        total_deaths = [deaths[day.strftime('%-m/%-d/%y')][rows].sum() for day in days]
-    return np.array(total_cases), np.array(total_recovered), np.array(total_deaths)
+def compute_IR(total,gamma=0.05):
+    """Determine (active) infected and recovered from total (I+R) time series."""
+    n = len(total)
+    M = np.zeros((n,n))
+    for i in range(n):
+        M[i,i] = 1.0
+        for j in range(i):
+            M[i,j] = gamma
+    I = np.linalg.solve(M,total)
+    R = total-I
+    return I, R
 
-population = {
-    'World' : 7.77e9,
-    'Austria': 8.822e6,
-    'France': 66.99e6,
-    'Germany': 82.79e6,
-    'Korea, South': 51.47e6,
-    'Italy' : 60.48e6,
-    'Netherlands': 17.18e6,
-    'Spain' : 46.66e6,
-    'Switzerland': 8.57e6,
-    'United Kingdom': 66.44e6,
-    'US' : 372.2e6    
-}
+def compute_IR_delay(total,delay=20):
+    """Determine (active) infected and recovered from total (I+R) time series."""
+    n = len(total)
+    M = np.zeros((n,n))
+    for i in range(n):
+        M[i,i] = 1.0
+        for j in range(i-delay):
+            M[i,j] = 1.
+    I = np.linalg.solve(M,total)
+    R = total-I
+    return I, R
+
 
 def SIR_mitigated(region='world', start_date=today, beta=0.25, gamma=0.05,\
                   confirmed=25, critical=10, fatal=2,
@@ -62,8 +44,8 @@ def SIR_mitigated(region='world', start_date=today, beta=0.25, gamma=0.05,\
                   Axis='Linear'):
     """ Model the current outbreak using the SIR model."""
 
-    total_cases, total_recovered, total_deaths = load_cases(region)
-    active_confirmed = total_cases - total_recovered - total_deaths
+    total_cases, total_deaths = load_cases(region)
+    active_confirmed, total_recovered = compute_IR(total_cases)
     confirmed_fraction = confirmed/100.
     N = population[region]
     
@@ -81,8 +63,8 @@ def SIR_mitigated(region='world', start_date=today, beta=0.25, gamma=0.05,\
         return du
 
     # Initial values
-    u0[2] = (total_recovered[-1]+total_deaths[-1])/confirmed_fraction  # Initial recovered
-    u0[1] = active_confirmed[-1]/confirmed_fraction-u0[2] # Initial infected
+    u0[2] = total_recovered[-1]/confirmed_fraction  # Initial recovered
+    u0[1] = active_confirmed[-1]/confirmed_fraction # Initial infected
     u0[0] = N - u0[1] - u0[2]
 
     T = 400
